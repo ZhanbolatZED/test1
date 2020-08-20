0b05c21c58c3050a279568de0960ddfa1e03af56
JEKI <62481414+ZhanbolatZED@users.noreply.github.com>
2020-08-20 22:04:11 +0600

    test1 public file code index html
    test test test test test test

---
+++ script.js
@@ -0,0 +1,58 @@
+// // alert("hello world");
+
+// var x = 30;
+// console.log("The value of x iso", x);
+
+// console.log(Math.max(3,5,5,3,6,1,534,2));
+// console.log(Math.max(32,7) + 200);
+// console.log(Math.min(Math.max(40,2) + Math.max(8,70)));
+
+// confirm("Shall we, then!");
+
+// prompt("oppo samsung htc smartfon moxom" , "micro, zed, lad, i'm sory");
+
+// var theNumber = Number(prompt("Выбери число", ""));
+// alert("Твоё число – квадратный корень из " + theNumber * theNumber);
+
+// var kankulator = Number(prompt("A x 2 =", ""));
+// alert("Result:" + kankulator * kankulator);
+
+// var theNumber = prompt("Выбери число ", "");
+// if (!isNaN(theNumber))
+// alert("Твоё число – квадратный корень из " + theNumber * theNumber);
+
+// var theNumber = Number(prompt("Выбери число", ""));
+// if (!isNaN(theNumber))
+// alert("Твоё число – квадратный корень из " + theNumber * theNumber);
+// else
+// alert("Ну ты что число-то не ввёл?");
+
+// var num = Number(prompt("Выбери число", "0"));
+// if (num < 10)
+// alert("Маловато");
+// else if (num < 100)
+// alert("Нормально");
+// else
+// alert("Многовато");
+
+// var number = 0;
+// while (number <= 1) {
+// 	console.log(number);
+// 	number = number + 0.1;
+// }
+
+// var result = 1;
+// var counter = 0;
+// while (counter < 10) {
+// result = result * 2;
+// counter = counter + 1;
+// }
+// console.log(result);
+// console.log(counter);
+// → 1024
+
+// do {
+// var name = prompt("Who are you?");
+// } while (!name);
+// console.log(name);
+
